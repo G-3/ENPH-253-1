9ad61849e85e42a8a1b7b2642e1201d5e31a6bb3
Cheng Xie <cheng.k.xie@gmail.com>
2016-07-18 16:55:18 -0700

    Added code to test all 10 QRDs

--- AutoR/software/software.ino
+++ AutoR/software/software.ino
@@ -1,5 +1,7 @@
 // This include statement should be changed to whatever we want to be running 
-#include "./test_nav/test_nav.h"
+
+// #include "./test_nav/test_nav.h"
+#include "./test_qrd/test_qrd.h"
 
 void setup()
 {
