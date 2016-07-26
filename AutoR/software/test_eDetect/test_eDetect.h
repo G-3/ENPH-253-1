56b6d25979d6e845f3904bfde6b4c01d39a29f11
Curtis Huebner <mathcurt@gmail.com>
2016-07-25 18:23:47 -0700

    Commented out print statements from event detector

--- AutoR/software/test_eDetect/test_eDetect.h
+++ AutoR/software/test_eDetect/test_eDetect.h
@@ -9,7 +9,19 @@
 
 #include "../Ai/Debug.h"
 #include "../Ai/LLRobot.h"
-#include "../EDetect.h"
+#include "../Ai/EDetect.h"
+
+#define ledPin 13
+
+LiquidCrystal LCD(26,27,28,16, 17, 18, 19,20,21,22,23);
+
+motorClass motor ;
+
+ServoTINAH RCServo0;    // declare variables for up to eight     servos.   Replaced old Servo253 implementation 2015Jan2
+ServoTINAH RCServo1; 
+ServoTINAH RCServo2;
+ServoTINAH RCServo3;
+
 
 using namespace LLRobot;
 void setup_m();
@@ -30,30 +42,35 @@ void setup_m(){
     delay(50);
 }
 
-void loop_m();
-{
+void loop_m(){
     testID();
 }
 
-void testID{
+void testID(){
     bool intersectionDetected = false;
     int16_t counter = 0;
     while(!startbutton()){
         delay(1);
-        counter++;
-        counter %= 100;
+        counter %= 10;
+        int32_t t1 = micros();
+        bool val = Event::checkIntersect();
+        int32_t t2 = micros();
+        intersectionDetected |= val;
         if (counter == 0){
             if (intersectionDetected == false){
                 LCD.clear(); LCD.home();
-                LCD.print("True");
+                LCD.print("False");
             }else{
                 LCD.clear(); LCD.home();
-                LCD.print("False");
+                LCD.print("True");
             }
+            LCD.setCursor(0,1);
+            LCD.print(t2-t1);
+
         }
-        if(Event::checkIntersect){
-            bool intersectionDetected = true;
-        }
+        
+        Serial.println(val);
+        counter++;
     }
     while(startbutton());
 }
